5b3b12f9b536588b1f0c6f181194cbbd21c9ccbf
InWamos <mykyta.pidgornyi@gmail.com>
2025-11-26 21:52:01 +0000

    add contexts for the user repository interface

--- internal/user/infrastructure/user_repository.go
+++ internal/user/infrastructure/user_repository.go
@@ -1,14 +1,16 @@
 package infrastructure
 
 import (
+	"context"
+
 	"github.com/InWamos/trinity-proto/internal/user/domain"
 	"github.com/google/uuid"
 )
 
 type UserRepository interface {
-	GetUserByID(id uuid.UUID) (domain.User, error)
-	GetUserByUsername(username string) (domain.User, error)
-	RemoveUserByID(id uuid.UUID) error
-	ChangeUserRoleById(id uuid.UUID, changeToRole domain.Role) error
-	CreateUser(user domain.User) error
+	GetUserByID(ctx context.Context, id uuid.UUID) (domain.User, error)
+	GetUserByUsername(ctx context.Context, username string) (domain.User, error)
+	RemoveUserByID(ctx context.Context, id uuid.UUID) error
+	ChangeUserRoleById(ctx context.Context, id uuid.UUID, changeToRole domain.Role) error
+	CreateUser(ctx context.Context, user domain.User) error
 }

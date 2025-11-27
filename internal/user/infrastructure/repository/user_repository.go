8df6a5bec1faa2a3d84a3f27a5c3ddbe34933e09
InWamos <mykyta.pidgornyi@gmail.com>
2025-11-27 19:17:13 +0000

    Fix typo in ID

--- internal/user/infrastructure/repository/user_repository.go
+++ internal/user/infrastructure/repository/user_repository.go
@@ -11,6 +11,6 @@ type UserRepository interface {
 	GetUserByID(ctx context.Context, id uuid.UUID) (domain.User, error)
 	GetUserByUsername(ctx context.Context, username string) (domain.User, error)
 	RemoveUserByID(ctx context.Context, id uuid.UUID) error
-	ChangeUserRoleById(ctx context.Context, id uuid.UUID, changeToRole domain.Role) error
+	ChangeUserRoleByID(ctx context.Context, id uuid.UUID, changeToRole domain.Role) error
 	CreateUser(ctx context.Context, user domain.User) error
 }

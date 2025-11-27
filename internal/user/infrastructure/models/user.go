7632bcf84fb7edbd919c3c53e0524a3357302bb2
InWamos <mykyta.pidgornyi@gmail.com>
2025-11-27 22:59:30 +0000

    remove mapper from model

--- internal/user/infrastructure/models/user.go
+++ internal/user/infrastructure/models/user.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 
-	"github.com/InWamos/trinity-proto/internal/user/domain"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
@@ -23,8 +22,4 @@ type UserModel struct {
 	Role         Role `gorm:"type:enum('user', 'admin')"`
 	CreatedAt    time.Time
 	DeletedAt    gorm.DeletedAt
-}
-
-func (model *UserModel) ToEntity() *domain.User {
-	return domain.NewUser(model.ID, model.Username, model.DisplayName, model.PasswordHash, domain.Role(model.Role))
 }

a4e3fafd5d71312c941f3245af31651d7ee8a91f
InWamos <mykyta.pidgornyi@gmail.com>
2025-11-27 22:53:47 +0000

    add constructor

--- internal/user/application/demote_user.go
+++ internal/user/application/demote_user.go
@@ -1,6 +1,8 @@
 package application
 
 import (
+	"context"
+
 	"github.com/InWamos/trinity-proto/internal/shared/interfaces"
 	"github.com/InWamos/trinity-proto/internal/user/infrastructure/repository"
 	"github.com/google/uuid"
@@ -13,4 +15,18 @@ type DemoteUserRequest struct {
 type DemoteUser struct {
 	transactionManager interfaces.TransactionManager
 	userRepository     repository.UserRepository
+}
+
+func NewDemoteUser(
+	transactionManager interfaces.TransactionManager,
+	userRepository repository.UserRepository,
+) *DemoteUser {
+	return &DemoteUser{
+		transactionManager: transactionManager,
+		userRepository:     userRepository,
+	}
+}
+
+func (i *DemoteUser) Execute(ctx context.Context, input DemoteUserRequest) error {
+	return nil
 }

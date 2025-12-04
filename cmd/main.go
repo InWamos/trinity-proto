1d2cdd2823ce46efa6b2b786213de71781426671
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-04 18:43:00 +0000

    provide cors and trusted proxy middlewares

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/InWamos/trinity-proto/config"
 	"github.com/InWamos/trinity-proto/logger"
+	"github.com/InWamos/trinity-proto/middleware"
 	"github.com/InWamos/trinity-proto/setup"
 	"go.uber.org/fx"
 )
@@ -13,6 +14,7 @@ func main() {
 	fx.New(
 		fx.Provide(config.NewDatabaseConfig, config.NewLoggingConfig, config.NewServerConfig),
 		fx.Provide(logger.GetLogger),
+		fx.Provide(middleware.NewGlobalCORSMiddleware, middleware.NewTrustedProxyMiddleware),
 		fx.Provide(setup.NewHTTPServer),
 		fx.Invoke(func(*http.Server) {}),
 	).Run()

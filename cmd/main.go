0a9e5e0d21ebbfa9e58028f228110b7aa514a413
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-04 15:23:16 +0000

    provide logger and configs

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"net/http"
 
+	"github.com/InWamos/trinity-proto/config"
+	"github.com/InWamos/trinity-proto/logger"
 	"github.com/InWamos/trinity-proto/setup"
 	"go.uber.org/fx"
 )
 
 func main() {
 	fx.New(
+		fx.Provide(config.NewDatabaseConfig, config.NewLoggingConfig, config.NewServerConfig),
+		fx.Provide(logger.GetLogger),
 		fx.Provide(setup.NewHTTPServer),
 		fx.Invoke(func(*http.Server) {}),
 	).Run()

368856c622179940c70c5c1e93dc86850a344950
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-04 15:07:16 +0000

    change gin prefix to server prefix

---
+++ config/server_config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type ServerConfig struct {
+	BindAddress   string `mapstructure:"SERVER_ADDRESS"`
+	Port          int    `mapstructure:"SERVER_PORT"`
+	Mode          string `mapstructure:"SERVER_MODE"`
+	TrustedProxy  string `mapstructure:"SERVER_TRUSTED_PROXY"`
+	AllowedOrigin string `mapstructure:"SERVER_ALLOWED_ORIGIN"`
+}
+
+func NewServerConfig() (*ServerConfig, error) {
+	var serverConfig ServerConfig
+	if err := viper.Unmarshal(&serverConfig); err != nil {
+		return nil, err
+	}
+	return &serverConfig, nil
+}

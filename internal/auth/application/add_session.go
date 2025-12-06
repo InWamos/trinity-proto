2c7b131b7c440c74a8b9a00e54740fb6465aad36
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-06 13:29:04 +0000

    add session body

--- internal/auth/application/add_session.go
+++ internal/auth/application/add_session.go
@@ -1,1 +1,20 @@
 package application
+
+import (
+	"log/slog"
+
+	"github.com/InWamos/trinity-proto/internal/auth/infrastructure"
+)
+
+type AddSession struct {
+	sessionRepository infrastructure.SessionRepository
+	logger            *slog.Logger
+}
+
+func NewAddSession(sessionRepository infrastructure.SessionRepository, logger *slog.Logger) *AddSession {
+	asLogger := logger.With(slog.String("component", "interactor"), slog.String("name", "add_session"))
+	return &AddSession{
+		sessionRepository: sessionRepository,
+		logger:            asLogger,
+	}
+}

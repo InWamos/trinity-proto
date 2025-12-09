f22a13b718becba1c05c3718813f1d48f64e191c
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-09 16:25:21 +0000

    application DI container

--- setup/user/application.go
+++ setup/user/application.go
@@ -1,1 +1,20 @@
 package user
+
+import (
+	"github.com/InWamos/trinity-proto/internal/user/application"
+	"github.com/InWamos/trinity-proto/internal/user/application/service"
+	"go.uber.org/fx"
+)
+
+func NewUserApplicationContainer() fx.Option {
+	return fx.Module(
+		"user_presentation",
+		fx.Provide(
+			// Provides password hasher
+			service.NewBcryptPasswordHasher,
+			// Provides uuid generator
+			service.NewUUIDGenerator,
+			application.NewCreateUser,
+		),
+	)
+}

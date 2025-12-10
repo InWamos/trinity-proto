0f722206766820c8935a79f482c90175517af33e
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-10 14:58:09 +0000

    add create user handler provider

--- setup/user/presentation.go
+++ setup/user/presentation.go
@@ -3,6 +3,7 @@ package user
 import (
 	"github.com/InWamos/trinity-proto/internal/user/presentation/service"
 	v1 "github.com/InWamos/trinity-proto/internal/user/presentation/v1"
+	"github.com/InWamos/trinity-proto/internal/user/presentation/v1/handlers"
 	"github.com/go-playground/validator/v10"
 	"go.uber.org/fx"
 )
@@ -15,6 +16,8 @@ func NewUserPresentationContainer() fx.Option {
 			NewValidator,
 			// Provides form validator
 			service.NewTagValidatePostFormValidator,
+			// Provides create user handler
+			handlers.NewCreateUserHandler,
 			// Provides User v1 api mux
 			v1.NewUserMuxV1,
 		),

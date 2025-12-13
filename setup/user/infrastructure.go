bc07b630f836a4031e149d109b431f5ac04e3f74
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-13 16:52:24 +0000

    simplify container injection logic

--- setup/user/infrastructure.go
+++ setup/user/infrastructure.go
@@ -4,7 +4,6 @@ import (
 	"github.com/InWamos/trinity-proto/internal/user/infrastructure/database"
 	gormrepository "github.com/InWamos/trinity-proto/internal/user/infrastructure/repository/gorm_repository"
 	"go.uber.org/fx"
-	"gorm.io/gorm"
 )
 
 func NewUserInfrastructureContainer() fx.Option {
@@ -15,18 +14,10 @@ func NewUserInfrastructureContainer() fx.Option {
 			gormrepository.NewGormMapper,
 			// Provides User repository
 			gormrepository.NewGormUserRepository,
-			// Provides Transaction manager
-			database.NewGormTransactionManager,
 			// Provides GORM session
-			database.NewGormTransaction,
+			database.NewGormTransactionFactory,
 			// Provides GormDatabase
 			database.NewGormDatabase,
-			// Provides *gorm.DB
-			NewGormDB,
 		),
 	)
-}
-
-func NewGormDB(gormDB *database.GormDatabase) *gorm.DB {
-	return gormDB.GetEngine()
 }

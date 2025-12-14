b4436ca94e9fd341ef51ed15d60df6426c9aa1bd
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-14 00:55:27 +0000

    add promote user interactor

--- internal/user/application/promote_user.go
+++ internal/user/application/promote_user.go
@@ -1,9 +1,13 @@
 package application
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"log/slog"
 
-	"github.com/InWamos/trinity-proto/internal/shared/interfaces"
+	"github.com/InWamos/trinity-proto/internal/user/domain"
+	"github.com/InWamos/trinity-proto/internal/user/infrastructure/database"
 	"github.com/InWamos/trinity-proto/internal/user/infrastructure/repository"
 	"github.com/google/uuid"
 )
@@ -13,7 +17,57 @@ type PromoteUserRequest struct {
 }
 
 type PromoteUser struct {
-	transactionManager interfaces.TransactionManager
-	userRepository     repository.UserRepository
-	logger             *slog.Logger
+	transactionManagerFactory database.TransactionManagerFactory
+	userRepositoryFactory     repository.UserRepositoryFactory
+	logger                    *slog.Logger
+}
+
+func NewPromoteUser(
+	transactionManagerFactory database.TransactionManagerFactory,
+	userRepositoryFactory repository.UserRepositoryFactory,
+	logger *slog.Logger,
+) *PromoteUser {
+	pulogger := logger.With(
+		slog.String("component", "interactor"),
+		slog.String("name", "promote_user"),
+	)
+	return &PromoteUser{
+		transactionManagerFactory: transactionManagerFactory,
+		userRepositoryFactory:     userRepositoryFactory,
+		logger:                    pulogger,
+	}
+}
+
+func (interactor *PromoteUser) Execute(ctx context.Context, input PromoteUserRequest) error {
+	interactor.logger.DebugContext(ctx, "Started PromoteUser execution", slog.String("user_id", input.ID.String()))
+
+	transactionManager, err := interactor.transactionManagerFactory.NewTransaction(ctx)
+	if err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to create transaction", slog.Any("err", err))
+		return ErrDatabaseFailed
+	}
+
+	userRepository := interactor.userRepositoryFactory.CreateUserRepositoryWithTransaction(transactionManager)
+
+	err = userRepository.ChangeUserRoleByID(ctx, input.ID, domain.RoleAdmin)
+	if err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to promote user", slog.Any("err", err))
+		if rollbackErr := transactionManager.Rollback(ctx); rollbackErr != nil {
+			interactor.logger.ErrorContext(ctx, "failed to rollback transaction", slog.Any("err", rollbackErr))
+		}
+		
+		// Check if the error is because the user was not found
+		if errors.Is(err, sql.ErrNoRows) || errors.Is(err, repository.ErrUserNotFound) {
+			return ErrUserNotFound
+		}
+		return ErrDatabaseFailed
+	}
+
+	if err = transactionManager.Commit(ctx); err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to commit", slog.Any("err", err))
+		return ErrDatabaseFailed
+	}
+
+	interactor.logger.DebugContext(ctx, "Finished PromoteUser execution")
+	return nil
 }

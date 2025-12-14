e9526b9e4fc37ffc28181540ae0785bf1ebbbd09
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-14 00:56:06 +0000

    add remove user interactor

--- internal/user/application/remove_user.go
+++ internal/user/application/remove_user.go
@@ -1,9 +1,12 @@
 package application
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"log/slog"
 
-	"github.com/InWamos/trinity-proto/internal/shared/interfaces"
+	"github.com/InWamos/trinity-proto/internal/user/infrastructure/database"
 	"github.com/InWamos/trinity-proto/internal/user/infrastructure/repository"
 	"github.com/google/uuid"
 )
@@ -13,7 +16,57 @@ type RemoveUserRequest struct {
 }
 
 type RemoveUser struct {
-	transactionManager interfaces.TransactionManager
-	userRepository     repository.UserRepository
-	logger             *slog.Logger
+	transactionManagerFactory database.TransactionManagerFactory
+	userRepositoryFactory     repository.UserRepositoryFactory
+	logger                    *slog.Logger
+}
+
+func NewRemoveUser(
+	transactionManagerFactory database.TransactionManagerFactory,
+	userRepositoryFactory repository.UserRepositoryFactory,
+	logger *slog.Logger,
+) *RemoveUser {
+	rulogger := logger.With(
+		slog.String("component", "interactor"),
+		slog.String("name", "remove_user"),
+	)
+	return &RemoveUser{
+		transactionManagerFactory: transactionManagerFactory,
+		userRepositoryFactory:     userRepositoryFactory,
+		logger:                    rulogger,
+	}
+}
+
+func (interactor *RemoveUser) Execute(ctx context.Context, input RemoveUserRequest) error {
+	interactor.logger.DebugContext(ctx, "Started RemoveUser execution", slog.String("user_id", input.ID.String()))
+
+	transactionManager, err := interactor.transactionManagerFactory.NewTransaction(ctx)
+	if err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to create transaction", slog.Any("err", err))
+		return ErrDatabaseFailed
+	}
+
+	userRepository := interactor.userRepositoryFactory.CreateUserRepositoryWithTransaction(transactionManager)
+
+	err = userRepository.RemoveUserByID(ctx, input.ID)
+	if err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to remove user", slog.Any("err", err))
+		if rollbackErr := transactionManager.Rollback(ctx); rollbackErr != nil {
+			interactor.logger.ErrorContext(ctx, "failed to rollback transaction", slog.Any("err", rollbackErr))
+		}
+		
+		// Check if the error is because the user was not found
+		if errors.Is(err, sql.ErrNoRows) || errors.Is(err, repository.ErrUserNotFound) {
+			return ErrUserNotFound
+		}
+		return ErrDatabaseFailed
+	}
+
+	if err = transactionManager.Commit(ctx); err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to commit", slog.Any("err", err))
+		return ErrDatabaseFailed
+	}
+
+	interactor.logger.DebugContext(ctx, "Finished RemoveUser execution")
+	return nil
 }

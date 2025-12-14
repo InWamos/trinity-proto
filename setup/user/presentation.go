8c6d6cb92eb53127f9f2c5f8b0764e8b97614358
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-14 01:09:20 +0000

    add new handlers

--- setup/user/presentation.go
+++ setup/user/presentation.go
@@ -18,6 +18,14 @@ func NewUserPresentationContainer() fx.Option {
 			service.NewTagValidatePostFormValidator,
 			// Provides create user handler
 			handlers.NewCreateUserHandler,
+			// Provides get user handler
+			handlers.NewGetUserHandler,
+			// Provides promote user handler
+			handlers.NewPromoteUserHandler,
+			// Provides demote user handler
+			handlers.NewDemoteUserHandler,
+			// Provides remove user handler
+			handlers.NewRemoveUserHandler,
 			// Provides User v1 api mux
 			v1.NewUserMuxV1,
 		),

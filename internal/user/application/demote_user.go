a620c8c4fcff6fa12de10e2ca3e821e35228c8bd
InWamos <mykyta.pidgornyi@gmail.com>
2025-12-14 00:55:15 +0000

    add demote use rinteractor

--- internal/user/application/demote_user.go
+++ internal/user/application/demote_user.go
@@ -2,9 +2,12 @@ package application
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"log/slog"
 
-	"github.com/InWamos/trinity-proto/internal/shared/interfaces"
+	"github.com/InWamos/trinity-proto/internal/user/domain"
+	"github.com/InWamos/trinity-proto/internal/user/infrastructure/database"
 	"github.com/InWamos/trinity-proto/internal/user/infrastructure/repository"
 	"github.com/google/uuid"
 )
@@ -14,14 +17,14 @@ type DemoteUserRequest struct {
 }
 
 type DemoteUser struct {
-	transactionManager interfaces.TransactionManager
-	userRepository     repository.UserRepository
-	logger             *slog.Logger
+	transactionManagerFactory database.TransactionManagerFactory
+	userRepositoryFactory     repository.UserRepositoryFactory
+	logger                    *slog.Logger
 }
 
 func NewDemoteUser(
-	transactionManager interfaces.TransactionManager,
-	userRepository repository.UserRepository,
+	transactionManagerFactory database.TransactionManagerFactory,
+	userRepositoryFactory repository.UserRepositoryFactory,
 	logger *slog.Logger,
 ) *DemoteUser {
 	dulogger := logger.With(
@@ -29,12 +32,42 @@ func NewDemoteUser(
 		slog.String("name", "demote_user"),
 	)
 	return &DemoteUser{
-		transactionManager: transactionManager,
-		userRepository:     userRepository,
-		logger:             dulogger,
+		transactionManagerFactory: transactionManagerFactory,
+		userRepositoryFactory:     userRepositoryFactory,
+		logger:                    dulogger,
 	}
 }
 
-func (i *DemoteUser) Execute(ctx context.Context, input DemoteUserRequest) error {
+func (interactor *DemoteUser) Execute(ctx context.Context, input DemoteUserRequest) error {
+	interactor.logger.DebugContext(ctx, "Started DemoteUser execution", slog.String("user_id", input.ID.String()))
+
+	transactionManager, err := interactor.transactionManagerFactory.NewTransaction(ctx)
+	if err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to create transaction", slog.Any("err", err))
+		return ErrDatabaseFailed
+	}
+
+	userRepository := interactor.userRepositoryFactory.CreateUserRepositoryWithTransaction(transactionManager)
+
+	err = userRepository.ChangeUserRoleByID(ctx, input.ID, domain.RoleUser)
+	if err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to demote user", slog.Any("err", err))
+		if rollbackErr := transactionManager.Rollback(ctx); rollbackErr != nil {
+			interactor.logger.ErrorContext(ctx, "failed to rollback transaction", slog.Any("err", rollbackErr))
+		}
+		
+		// Check if the error is because the user was not found
+		if errors.Is(err, sql.ErrNoRows) || errors.Is(err, repository.ErrUserNotFound) {
+			return ErrUserNotFound
+		}
+		return ErrDatabaseFailed
+	}
+
+	if err = transactionManager.Commit(ctx); err != nil {
+		interactor.logger.ErrorContext(ctx, "failed to commit", slog.Any("err", err))
+		return ErrDatabaseFailed
+	}
+
+	interactor.logger.DebugContext(ctx, "Finished DemoteUser execution")
 	return nil
 }

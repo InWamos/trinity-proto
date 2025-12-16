a7fe7833a60f5a3cc14572ca39aa3b13a1390a0e
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-16 12:17:35 +0000

    add on some custom errors

--- internal/shared/interfaces/user/client/user_client.go
+++ internal/shared/interfaces/user/client/user_client.go
@@ -1,6 +1,15 @@
 package client
 
-import "context"
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrUsernameAbsent    = errors.New("username record absent")
+	ErrPasswordMissmatch = errors.New("password missmatch")
+	ErrUnexpectedError   = errors.New("unexpected error occured")
+)
 
 type UserClient interface {
 	VerifyCredentials(ctx context.Context, username, password string) error

15ea435c68b640ade41365652c53c85b5eaebb3c
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-16 12:17:52 +0000

    add VerifyCredentials implementation

--- internal/user/presentation/client/user_client.go
+++ internal/user/presentation/client/user_client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 
 	"github.com/InWamos/trinity-proto/internal/shared/interfaces/user/client"
@@ -20,4 +21,16 @@ func NewUserClient(
 	return &UserClient{validateUserCredentialsInteractor: validateUserCredentialsInteractor, logger: logger}
 }
 
-func (client *UserClient) VerifyCredentials(ctx context.Context, username, password string) error
+func (uClient *UserClient) VerifyCredentials(ctx context.Context, username, password string) error {
+	interactorRequest := application.ValidateUserCredentialsRequest{Username: username, Password: password}
+	if err := uClient.validateUserCredentialsInteractor.Execute(ctx, interactorRequest); err != nil {
+		if errors.Is(err, application.ErrPasswordMismatch) {
+			return client.ErrPasswordMissmatch
+		}
+		if errors.Is(err, application.ErrUsernameAbsent) {
+			return client.ErrUsernameAbsent
+		}
+		return client.ErrUnexpectedError
+	}
+	return nil
+}

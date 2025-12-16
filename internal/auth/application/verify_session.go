4e4112a0a2e9b6408792cc314f46ae696eefb032
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-16 12:38:24 +0000

    init session verify

--- internal/auth/application/verify_session.go
+++ internal/auth/application/verify_session.go
@@ -1,1 +1,16 @@
 package application
+
+import (
+	"log/slog"
+
+	"github.com/InWamos/trinity-proto/internal/shared/interfaces/user/client"
+)
+
+type VerifySessionRequest struct {
+	Session_id string
+}
+
+type VerifySession struct {
+	userClient client.UserClient
+	logger     *slog.Logger
+}

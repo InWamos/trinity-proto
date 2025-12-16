3947acfb700df5fa83e2781a02ad2a1cce3eed73
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-16 11:58:13 +0000

    add user client implementation

--- internal/user/presentation/client/user_client.go
+++ internal/user/presentation/client/user_client.go
@@ -1,4 +1,23 @@
 package client
 
+import (
+	"context"
+	"log/slog"
+
+	"github.com/InWamos/trinity-proto/internal/shared/interfaces/user/client"
+	"github.com/InWamos/trinity-proto/internal/user/application"
+)
+
 type UserClient struct {
+	validateUserCredentialsInteractor *application.ValidateUserCredentials
+	logger                            *slog.Logger
 }
+
+func NewUserClient(
+	validateUserCredentialsInteractor *application.ValidateUserCredentials,
+	logger *slog.Logger,
+) client.UserClient {
+	return &UserClient{validateUserCredentialsInteractor: validateUserCredentialsInteractor, logger: logger}
+}
+
+func (client *UserClient) VerifyCredentials(ctx context.Context, username, password string) error

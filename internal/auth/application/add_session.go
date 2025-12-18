7f3aa7ef8a07b3c5e81c8fc242dba435932406fd
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-18 15:25:15 +0000

    add response DTO

--- internal/auth/application/add_session.go
+++ internal/auth/application/add_session.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"time"
 
 	"github.com/InWamos/trinity-proto/internal/auth/domain"
 	"github.com/InWamos/trinity-proto/internal/auth/infrastructure"
@@ -15,11 +16,17 @@ var (
 	ErrUnexpected         = errors.New("unexpected error")
 )
 
+const DefaultSessionDuration = 24 * time.Hour
+
 type AddSessionRequest struct {
 	Username  string
 	Password  string
 	IpAddress string
 	UserAgent string
+}
+
+type AddSessionResponse struct {
+	Session domain.Session
 }
 
 type AddSession struct {
@@ -41,32 +48,46 @@ func NewAddSession(
 	}
 }
 
-func (asInteractor *AddSession) Execute(ctx context.Context, input AddSessionRequest) error {
+func (asInteractor *AddSession) Execute(ctx context.Context, input AddSessionRequest) (AddSessionResponse, error) {
 	// Check whether the provided credentials valid
 	response, err := asInteractor.userClient.VerifyCredentials(ctx, input.Username, input.Password)
 	if err != nil {
 		switch {
 		case errors.Is(err, client.ErrUsernameAbsent):
 			asInteractor.logger.InfoContext(ctx, "Username doesn't exist", slog.String("username", input.Username))
-			return ErrInvalidCredentials
+			return AddSessionResponse{}, ErrInvalidCredentials
 		case errors.Is(err, client.ErrPasswordMissmatch):
 			asInteractor.logger.InfoContext(ctx, "Password didn't match", slog.String("username", input.Username))
-			return ErrInvalidCredentials
+			return AddSessionResponse{}, ErrInvalidCredentials
 		default:
 			asInteractor.logger.ErrorContext(ctx, "Unexpected error during credential verification")
-			return ErrUnexpected
+			return AddSessionResponse{}, ErrUnexpected
 		}
 	}
+
 	newSession, err := domain.NewSession(
 		response.UserID,
 		domain.UserRole(response.UserRole),
 		input.IpAddress,
 		input.UserAgent,
 		"",
-		0,
+		DefaultSessionDuration,
 	)
-	asInteractor.sessionRepository.CreateSession(ctx, *newSession)
-	return nil
-}
+	if err != nil {
+		asInteractor.logger.ErrorContext(ctx, "Failed to create new session", slog.Any("err", err))
+		return AddSessionResponse{}, ErrUnexpected
+	}
 
-// TODO add Execute implementation to this interactor
+	err = asInteractor.sessionRepository.CreateSession(ctx, *newSession)
+	if err != nil {
+		asInteractor.logger.ErrorContext(ctx, "Failed to save session", slog.Any("err", err))
+		return AddSessionResponse{}, ErrUnexpected
+	}
+
+	asInteractor.logger.InfoContext(ctx, "Session created successfully",
+		slog.String("user_id", response.UserID.String()),
+		slog.String("session_id", newSession.ID.String()),
+	)
+
+	return AddSessionResponse{Session: *newSession}, nil
+}

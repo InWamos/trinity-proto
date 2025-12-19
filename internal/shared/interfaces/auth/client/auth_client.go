e339b94190828846f6d9790592f867844801ce00
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-19 17:20:58 +0000

    add sentinel errors to auth client interface

--- internal/shared/interfaces/auth/client/auth_client.go
+++ internal/shared/interfaces/auth/client/auth_client.go
@@ -1,6 +1,16 @@
 package client
 
-import "context"
+import (
+	"context"
+	"errors"
+)
+
+var (
+	ErrSessionInvalid  = errors.New("session invalid or not found")
+	ErrSessionExpired  = errors.New("session expired")
+	ErrSessionRevoked  = errors.New("session revoked")
+	ErrUnexpectedError = errors.New("unexpected error occurred")
+)
 
 type AuthClient interface {
 	ValidateSession(ctx context.Context, session string) error

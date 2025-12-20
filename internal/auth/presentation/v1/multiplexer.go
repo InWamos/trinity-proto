08be4a5616346d74ca9833f9b00ecb7ca487dc22
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-20 23:49:00 +0000

    change log in method to post

--- internal/auth/presentation/v1/multiplexer.go
+++ internal/auth/presentation/v1/multiplexer.go
@@ -4,21 +4,22 @@ import (
 	"net/http"
 
 	"github.com/InWamos/trinity-proto/internal/auth/presentation/v1/handlers"
+	"github.com/InWamos/trinity-proto/middleware"
 )
 
 type AuthMuxV1 struct {
 	mux *http.ServeMux
 }
 
-func NewUserMuxV1(
+func NewAuthMuxV1(
 	loginHandler *handlers.LoginHandler,
+	authMiddleware *middleware.AuthenticationMiddleware,
 ) *AuthMuxV1 {
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /", loginHandler.ServeHTTP)
+	mux.HandleFunc("POST /login", loginHandler.ServeHTTP)
 	return &AuthMuxV1{mux: mux}
 }
 
-func (um *AuthMuxV1) GetMux() *http.ServeMux {
-	return um.mux
+func (am *AuthMuxV1) GetMux() *http.ServeMux {
+	return am.mux
 }

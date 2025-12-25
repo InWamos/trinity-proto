f7442e223cf844cf2211e993612ce94a1982a51f
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-25 19:45:49 +0000

    remove unused argument

--- internal/auth/presentation/v1/multiplexer.go
+++ internal/auth/presentation/v1/multiplexer.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/InWamos/trinity-proto/internal/auth/presentation/v1/handlers"
-	"github.com/InWamos/trinity-proto/middleware"
 )
 
 type AuthMuxV1 struct {
@@ -13,7 +12,6 @@ type AuthMuxV1 struct {
 
 func NewAuthMuxV1(
 	loginHandler *handlers.LoginHandler,
-	authMiddleware *middleware.AuthenticationMiddleware,
 ) *AuthMuxV1 {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /login", loginHandler.ServeHTTP)

1c8dc2d9207c41e797fdbe8ad03df0c26750950b
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-26 21:46:48 +0000

    fix error type evaluation

--- internal/user/application/service/role_based_authorization_test.go
+++ internal/user/application/service/role_based_authorization_test.go
@@ -102,7 +102,7 @@ func TestAuthorizeByRoleEdgeCases(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := AuthorizeByRole(tt.identity, tt.requiredRole)
+			err := service.AuthorizeByRole(tt.identity, tt.requiredRole)
 
 			if tt.expectError {
 				if err == nil {

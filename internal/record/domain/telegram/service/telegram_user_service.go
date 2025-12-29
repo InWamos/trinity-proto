fd09f7739784aaead8c900c073d20d982bb6f1e4
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-29 23:04:53 +0000

    add service for TElegramUser model

--- internal/record/domain/telegram/service/telegram_user_service.go
+++ internal/record/domain/telegram/service/telegram_user_service.go
@@ -1,4 +1,22 @@
 package service
 
-type TelegramUsersService struct {
+import (
+	domain "github.com/InWamos/trinity-proto/internal/record/domain/telegram"
+	"github.com/go-playground/validator/v10"
+)
+
+type TelegramUserService struct {
+	validator *validator.Validate
+}
+
+func NewTelegramUserService(validator *validator.Validate) *TelegramUserService {
+	return &TelegramUserService{validator: validator}
+}
+
+func (service *TelegramUserService) Validate(telegramUser domain.TelegramUser) error {
+	err := service.validator.Struct(telegramUser)
+	if err != nil {
+		return domain.ErrValidationFailed
+	}
+	return nil
 }

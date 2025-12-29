93d4778ceb97abf1877da276e55b0056ca959c4e
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-29 23:27:02 +0000

    fix username lower boundary

--- internal/record/domain/telegram/telegram_user.go
+++ internal/record/domain/telegram/telegram_user.go
@@ -9,7 +9,7 @@ import (
 type TelegramUser struct {
 	ID              uuid.UUID `validate:"required,uuid"`
 	TelegramID      uint64    `validate:"required,gt=0,lte=300000000000"`
-	CurrentUsername string    `validate:"required,min=5,max=32"`
+	CurrentUsername string    `validate:"required,min=4,max=32"`
 	Bio             string    `validate:"max=200"`
 	AddedAt         time.Time `validate:"required"`
 	AddedByUser     uuid.UUID `validate:"uuid"`

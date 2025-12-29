c71cbd5319caaf2885ac4f8555918bf7fcc0faba
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-29 23:31:41 +0000

    add First name and last name attributes

--- internal/record/domain/telegram/telegram_user.go
+++ internal/record/domain/telegram/telegram_user.go
@@ -6,10 +6,13 @@ import (
 	"github.com/google/uuid"
 )
 
+// TelegramUser Validation domain rules according to https://limits.tginfo.me/en.
 type TelegramUser struct {
 	ID              uuid.UUID `validate:"required,uuid"`
 	TelegramID      uint64    `validate:"required,gt=0,lte=300000000000"`
 	CurrentUsername string    `validate:"required,min=4,max=32"`
+	FirstName       string    `validate:"min=1,max=64"`
+	LastName        string    `validate:"min=0,max=64"`
 	Bio             string    `validate:"max=140"`
 	AddedAt         time.Time `validate:"required"`
 	AddedByUser     uuid.UUID `validate:"uuid"`

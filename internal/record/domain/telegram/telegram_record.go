c5c16d1e8dc62af8b52bf7d9a43a4a67053f5825
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-29 23:36:00 +0000

    update max message length

--- internal/record/domain/telegram/telegram_record.go
+++ internal/record/domain/telegram/telegram_record.go
@@ -9,7 +9,7 @@ import (
 type TelegramRecord struct {
 	ID          uuid.UUID `validate:"required,uuid"`
 	Author      uuid.UUID `validate:"required,uuid"`
-	MessageText string    `validate:"required,min=1,max"`
+	MessageText string    `validate:"required,min=1,max=4096"`
 	Attachments []uuid.UUID
 	PostedAt    time.Time `validate:"required"`
 	AddedByUser uuid.UUID `validate:"required,uuid"`

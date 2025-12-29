adede7a3766ec34afb1a6588eff115fe24bc7613
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-29 23:29:21 +0000

    Fix bio length according to telegram limits

--- internal/record/domain/telegram/telegram_user.go
+++ internal/record/domain/telegram/telegram_user.go
@@ -10,7 +10,7 @@ type TelegramUser struct {
 	ID              uuid.UUID `validate:"required,uuid"`
 	TelegramID      uint64    `validate:"required,gt=0,lte=300000000000"`
 	CurrentUsername string    `validate:"required,min=4,max=32"`
-	Bio             string    `validate:"max=200"`
+	Bio             string    `validate:"max=140"`
 	AddedAt         time.Time `validate:"required"`
 	AddedByUser     uuid.UUID `validate:"uuid"`
 }

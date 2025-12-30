0c4adf5ddb020eb57690d790255055f197d056f0
InWamos <74014248+InWamos@users.noreply.github.com>
2025-12-30 10:34:03 +0000

    update Record domain model

--- internal/record/domain/telegram/telegram_record.go
+++ internal/record/domain/telegram/telegram_record.go
@@ -10,11 +10,12 @@ import (
 // this beautiful implementation of MTProto client
 // https://github.com/KurimuzonAkuma/kurigram/blob/dev/pyrogram/types/messages_and_media/message.py
 type TelegramRecord struct {
-	ID          uuid.UUID `validate:"required,uuid"`
-	Author      uuid.UUID `validate:"required,uuid"`
-	MessageText string    `validate:"required,min=1,max=4096"`
-	Attachments []uuid.UUID
-	PostedAt    time.Time `validate:"required"`
-	AddedAt     time.Time `validate:"required"`
-	AddedByUser uuid.UUID `validate:"required,uuid"`
+	ID                 uuid.UUID `validate:"required,uuid"`
+	FromUserTelegramID uint64    `validate:"required,gt=0,lte=300000000000"`
+	InTelegramChatID   int64     `validate:"required"`
+	MessageText        string    `validate:"required,max=4096"`
+	Attachments        []uuid.UUID
+	PostedAt           time.Time `validate:"required"`
+	AddedAt            time.Time `validate:"required"`
+	AddedByUser        uuid.UUID `validate:"required,uuid"`
 }

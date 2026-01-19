edf73f80e76f41c6b7e676b58ff835d608269d29
InWamos <74014248+InWamos@users.noreply.github.com>
2026-01-19 15:19:30 +0000

    add constructor for getter interactor

--- internal/record/application/telegram/get_latest_telegram_records_by_telegram_id.go
+++ internal/record/application/telegram/get_latest_telegram_records_by_telegram_id.go
@@ -3,6 +3,7 @@ package application
 import (
 	"log/slog"
 
+	domain "github.com/InWamos/trinity-proto/internal/record/domain/telegram"
 	"github.com/InWamos/trinity-proto/internal/record/infrastructure/repository"
 	"github.com/InWamos/trinity-proto/internal/shared/interfaces"
 )
@@ -14,4 +15,25 @@ type GetLatestTelegramRecordsByUserTelegramID struct {
 }
 
 type GetLatestTelegramRecordsByUserTelegramIDRequest struct {
+	UserTelegramID uint64
+}
+
+type GetLatestTelegramRecordsByUserTelegramIDResponse struct {
+	TelegramRecords []domain.TelegramRecord
+}
+
+func NewGetLatestTelegramRecordsByUserTelegramID(
+	transactionManagerFactory interfaces.TransactionManagerFactory,
+	telegramRecordFactory repository.TelegramRecordRepositoryFactory,
+	logger *slog.Logger,
+) *GetLatestTelegramRecordsByUserTelegramID {
+	iLogger := logger.With(
+		slog.String("module", "record"),
+		slog.String("name", "get_latest_tg_records_by_user_telegram_id"),
+	)
+	return &GetLatestTelegramRecordsByUserTelegramID{
+		transactionManagerFactory: transactionManagerFactory,
+		telegramRecordFactory:     telegramRecordFactory,
+		logger:                    iLogger,
+	}
 }

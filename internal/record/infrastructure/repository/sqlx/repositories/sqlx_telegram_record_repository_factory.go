43ca92ae96aaccba3e3440b8a2be512e2a97e808
InWamos <74014248+InWamos@users.noreply.github.com>
2026-01-21 15:27:06 +0000

    add sqlx record factory implementation

---
+++ internal/record/infrastructure/repository/sqlx/repositories/sqlx_telegram_record_repository_factory.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+	"log/slog"
+
+	"github.com/InWamos/trinity-proto/internal/record/infrastructure/repository"
+	"github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/mappers"
+	"github.com/InWamos/trinity-proto/internal/shared/interfaces"
+	"github.com/jmoiron/sqlx"
+)
+
+type SQLXTelegramRecordRepositoryFactory struct {
+	logger     *slog.Logger
+	sqlxMapper *mappers.SqlxTelegramRecordMapper
+}
+
+func (factory *SQLXTelegramRecordRepositoryFactory) NewSqlxUserRepositoryFactory(
+	logger *slog.Logger,
+	mapper *mappers.SqlxTelegramRecordMapper,
+) repository.TelegramRecordRepositoryFactory {
+	return &SQLXTelegramRecordRepositoryFactory{
+		logger:     logger,
+		sqlxMapper: mapper,
+	}
+}
+
+func (factory *SQLXTelegramRecordRepositoryFactory) CreateTelegramRecordRepositoryWithTransaction(
+	tm interfaces.TransactionManager,
+) repository.TelegramRecordRepository {
+	tx, ok := tm.GetTransaction().(*sqlx.Tx)
+	if !ok {
+		factory.logger.Error("invalid transaction type, expected *sqlx.Tx")
+		panic("invalid transaction type for sqlx repository")
+	}
+	return &SQLXTelegramRecordRepository{
+		session:    tx,
+		sqlxMapper: factory.sqlxMapper,
+		logger:     factory.logger,
+	}
+}

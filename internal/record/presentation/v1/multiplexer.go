61904f831afac3216cc43efc355e34c057367805
InWamos <74014248+InWamos@users.noreply.github.com>
2026-01-31 12:27:31 +0000

    add get records by telegram id handler

--- internal/record/presentation/v1/multiplexer.go
+++ internal/record/presentation/v1/multiplexer.go
@@ -1,13 +1,19 @@
 package v1
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/InWamos/trinity-proto/internal/record/presentation/v1/handlers"
+	"github.com/go-chi/chi/v5"
+)
 
 type RecordMuxV1 struct {
 	mux *chi.Mux
 }
 
-func NewRecordMuxV1() *RecordMuxV1 {
+func NewRecordMuxV1(
+	getLatestTelegramRecordsByTelegramID *handlers.GetLatestTelegramRecordsByTelegramIDHandler,
+) *RecordMuxV1 {
 	mux := chi.NewRouter()
+	mux.Get("/telegram/{telegram_id}/records", getLatestTelegramRecordsByTelegramID.ServeHTTP)
 	return &RecordMuxV1{
 		mux: mux,
 	}

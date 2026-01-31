50d8fb68a5f2bdb8d24700e6bc80add37ce2c632
InWamos <74014248+InWamos@users.noreply.github.com>
2026-01-31 13:12:46 +0000

    add telegram user table

--- internal/record/infrastructure/migrations/000001_create_records_table.up.sql
+++ internal/record/infrastructure/migrations/000001_create_records_table.up.sql
@@ -5,7 +5,8 @@ CREATE SCHEMA IF NOT EXISTS "records";
 
 CREATE TABLE IF NOT EXISTS "records"."telegram_records" (
     id UUID PRIMARY KEY NOT NULL,
-    from_user_telegram_id BIGINT NOT NULL,
+    from_user_telegram_id BIGINT
+    REFERENCES "records"."telegram_users" (telegram_id),
     in_telegram_chat_id BIGINT NOT NULL,
     message_text TEXT,
     posted_at TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -13,6 +14,26 @@ CREATE TABLE IF NOT EXISTS "records"."telegram_records" (
     added_by_user UUID NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS "records"."telegram_users" (
+    id UUID PRIMARY KEY NOT NULL,
+    telegram_id BIGINT NOT NULL,
+    telegram_user_identity_id
+    UUID REFERENCES "records"."telegram_user_identities" (id),
+    added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    added_by_user UUID NOT NULL,
+    UNIQUE (telegram_id, added_by_user)
+);
+
+CREATE TABLE IF NOT EXISTS "records"."telegram_user_identities" (
+    id UUID PRIMARY KEY NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    username TEXT,
+    phone_number TEXT,
+    bio TEXT,
+    added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    added_by_user UUID NOT NULL
+);
 -- squawk-ignore require-concurrent-index-creation
 CREATE INDEX IF NOT EXISTS
 idx_from_user_telegram_id ON "records".telegram_record (from_user_telegram_id);

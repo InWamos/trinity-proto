1bd66fb6f2ee03614fd58106cfcd63e74ed657fe
InWamos <74014248+InWamos@users.noreply.github.com>
2026-01-31 18:05:11 +0000

    sqlx user mapper

---
+++ internal/record/infrastructure/repository/sqlx/mappers/sqlx_telegram_user_mapper.go
@@ -0,0 +1,32 @@
+package mappers
+
+import (
+	domain "github.com/InWamos/trinity-proto/internal/record/domain/telegram"
+	"github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/models"
+)
+
+type SqlxTelegramUserMapper struct{}
+
+func NewSqlxTelegramUserMapper() *SqlxTelegramUserMapper {
+	return &SqlxTelegramUserMapper{}
+}
+
+func (sm *SqlxTelegramUserMapper) ToDomain(inputModel models.TelegramUserModel) domain.TelegramUser {
+	return domain.TelegramUser{
+		ID:               inputModel.ID,
+		TelegramID:       inputModel.TelegramID,
+		TelegramIdentity: inputModel.TelegramIdentityID,
+		AddedAt:          inputModel.AddedAt,
+		AddedByUser:      inputModel.AddedByUser,
+	}
+}
+
+func (sm *SqlxTelegramUserMapper) ToModel(inputEntity domain.TelegramUser) models.TelegramUserModel {
+	return models.TelegramUserModel{
+		ID:                 inputEntity.ID,
+		TelegramID:         inputEntity.TelegramID,
+		TelegramIdentityID: inputEntity.TelegramIdentity,
+		AddedAt:            inputEntity.AddedAt,
+		AddedByUser:        inputEntity.AddedByUser,
+	}
+}

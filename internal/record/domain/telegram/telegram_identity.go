c5eb255d49386123a7b7210712e0fd1ee9d374c4
InWamos <74014248+InWamos@users.noreply.github.com>
2026-01-31 13:26:55 +0000

    move all identity related data to a separate domain entity

---
+++ internal/record/domain/telegram/telegram_identity.go
@@ -0,0 +1,18 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TelegramIdentity struct {
+	ID          uuid.UUID `validate:"required,uuid"`
+	Username    string    `validate:"required,min=4,max=32"`
+	FirstName   string    `validate:"required,min=1,max=64"`
+	LastName    string    `validate:"min=0,max=64"`
+	Bio         string    `validate:"max=140"`
+	PhoneNumber string    `validate:"e164"`
+	AddedAt     time.Time `validate:"required"`
+	AddedByUser uuid.UUID `validate:"uuid"`
+}
--- internal/record/domain/telegram/telegram_user.go
+++ internal/record/domain/telegram/telegram_user.go
@@ -14,12 +14,9 @@ var (
 
 // TelegramUser Validation domain rules according to https://limits.tginfo.me/en.
 type TelegramUser struct {
-	ID              uuid.UUID `validate:"required,uuid"`
-	TelegramID      uint64    `validate:"required,gt=0,lte=300000000000"`
-	CurrentUsername string    `validate:"required,min=4,max=32"`
-	FirstName       string    `validate:"min=1,max=64"`
-	LastName        string    `validate:"min=0,max=64"`
-	Bio             string    `validate:"max=140"`
-	AddedAt         time.Time `validate:"required"`
-	AddedByUser     uuid.UUID `validate:"uuid"`
+	ID               uuid.UUID `validate:"required,uuid"`
+	TelegramID       uint64    `validate:"required,gt=0,lte=300000000000"`
+	TelegramIdentity uuid.UUID `validate:"required,uuid"`
+	AddedAt          time.Time `validate:"required"`
+	AddedByUser      uuid.UUID `validate:"uuid"`
 }

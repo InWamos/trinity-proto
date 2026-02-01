8544d447fb9e94e0a4f7e0b52ba899ecd0578f30
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-01 11:19:48 +0000

    add sqlx mapper for telegram identity model

---
+++ internal/record/infrastructure/repository/sqlx/mappers/sqlx_telegram_identity_mapper.go
@@ -0,0 +1,38 @@
+package mappers
+
+import (
+	domain "github.com/InWamos/trinity-proto/internal/record/domain/telegram"
+	"github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/models"
+)
+
+type SqlxTelegramIdentityMapper struct{}
+
+func NewSqlxTelegramIdentityMapper() *SqlxTelegramIdentityMapper {
+	return &SqlxTelegramIdentityMapper{}
+}
+
+func (sm *SqlxTelegramIdentityMapper) ToDomain(inputModel models.TelegramIdentityModel) domain.TelegramIdentity {
+	return domain.TelegramIdentity{
+		ID:          inputModel.ID,
+		FirstName:   inputModel.FirstName,
+		LastName:    inputModel.LastName,
+		Username:    inputModel.Username,
+		PhoneNumber: inputModel.PhoneNumber,
+		Bio:         inputModel.Bio,
+		AddedAt:     inputModel.AddedAt,
+		AddedByUser: inputModel.AddedByUser,
+	}
+}
+
+func (sm *SqlxTelegramIdentityMapper) ToModel(inputEntity domain.TelegramIdentity) models.TelegramIdentityModel {
+	return models.TelegramIdentityModel{
+		ID:          inputEntity.ID,
+		FirstName:   inputEntity.FirstName,
+		LastName:    inputEntity.LastName,
+		Username:    inputEntity.Username,
+		PhoneNumber: inputEntity.PhoneNumber,
+		Bio:         inputEntity.Bio,
+		AddedAt:     inputEntity.AddedAt,
+		AddedByUser: inputEntity.AddedByUser,
+	}
+}

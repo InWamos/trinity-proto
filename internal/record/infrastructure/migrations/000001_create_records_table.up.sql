13d872f60780effd994c8dd2e20647ac6ce7f896
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-05 13:27:55 +0000

    move users table to the top

--- internal/record/infrastructure/migrations/000001_create_records_table.up.sql
+++ internal/record/infrastructure/migrations/000001_create_records_table.up.sql
@@ -3,23 +3,24 @@ SET statement_timeout = '5s';
 SET lock_timeout = '1s';
 CREATE SCHEMA IF NOT EXISTS "records";
 
-CREATE TABLE IF NOT EXISTS "records"."telegram_records" (
-    id UUID PRIMARY KEY NOT NULL,
-    from_user_telegram_id BIGINT
-    REFERENCES "records"."telegram_users" (telegram_id),
-    in_telegram_chat_id BIGINT NOT NULL,
-    message_text TEXT,
-    posted_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    added_by_user UUID NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS "records"."telegram_users" (
     id UUID PRIMARY KEY NOT NULL,
     telegram_id BIGINT NOT NULL,
     added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     added_by_user UUID NOT NULL,
     UNIQUE (telegram_id, added_by_user)
+);
+
+CREATE TABLE IF NOT EXISTS "records"."telegram_records" (
+    id UUID PRIMARY KEY NOT NULL,
+    message_id BIGINT NOT NULL UNIQUE,
+    from_telegram_user_id UUID
+    REFERENCES "records".telegram_users (id),
+    in_telegram_chat_id BIGINT NOT NULL,
+    message_text TEXT,
+    posted_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    added_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    added_by_user UUID NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "records"."telegram_identities" (

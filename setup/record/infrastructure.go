7a52714a58ee3af6103de4cbffc4961ce06a3b6e
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-11 13:10:47 +0000

    add: fx telegram identity repositories

--- setup/record/infrastructure.go
+++ setup/record/infrastructure.go
@@ -2,6 +2,7 @@ package record
 
 import (
 	"github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/mappers"
+	SqlxTelegramIdentityRepositories "github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/repositories/telegram_identity"
 	SqlxTelegramRecordRepositories "github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/repositories/telegram_record"
 	SqlxTelegramUserRepositories "github.com/InWamos/trinity-proto/internal/record/infrastructure/repository/sqlx/repositories/telegram_user"
 	"go.uber.org/fx"
@@ -17,6 +18,8 @@ func NewRecordInfrastructureContainer() fx.Option {
 			SqlxTelegramRecordRepositories.NewSQLXTelegramRecordRepositoryFactory,
 			SqlxTelegramUserRepositories.NewSQLXTelegramUserRepository,
 			SqlxTelegramUserRepositories.NewSQLXTelegramUserRepositoryFactory,
+			SqlxTelegramIdentityRepositories.NewSQLXTelegramIdentityRepository,
+			SqlxTelegramIdentityRepositories.NewSQLXTelegramIdentityRepositoryFactory,
 		),
 	)
 }

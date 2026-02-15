b5c240a9ad33eb0d212f89bd9588095c364e07df
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-15 15:53:53 +0000

    add: add telegram record post endpoint to the router

--- internal/record/presentation/v1/multiplexer.go
+++ internal/record/presentation/v1/multiplexer.go
@@ -13,11 +13,13 @@ func NewRecordMuxV1(
 	getLatestTelegramRecordsByTelegramID *handlers.GetLatestTelegramRecordsByTelegramIDHandler,
 	addTelegramUser *handlers.AddTelegramUserHandler,
 	addTelegramIdentity *handlers.AddTelegramIdentityHandler,
+	addTelegramRecord *handlers.AddTelegramRecordHandler,
 ) *RecordMuxV1 {
 	mux := chi.NewRouter()
 	mux.Get("/telegram/{telegram_id}/records", getLatestTelegramRecordsByTelegramID.ServeHTTP)
 	mux.Post("/telegram/identity", addTelegramIdentity.ServeHTTP)
 	mux.Post("/telegram/user", addTelegramUser.ServeHTTP)
+	mux.Post("/telegram/record", addTelegramRecord.ServeHTTP)
 	return &RecordMuxV1{
 		mux: mux,
 	}

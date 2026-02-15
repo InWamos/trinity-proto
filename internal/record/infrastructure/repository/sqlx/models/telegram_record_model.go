8b12196beb8c4c09198f4d27d7f3a79558f3d7a2
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-15 15:26:12 +0000

    add: new message telegram id field

--- internal/record/infrastructure/repository/sqlx/models/telegram_record_model.go
+++ internal/record/infrastructure/repository/sqlx/models/telegram_record_model.go
@@ -8,6 +8,7 @@ import (
 
 type SQLXTelegramRecordModel struct {
 	ID                 uuid.UUID `db:"id"`
+	MessageTelegramID  uint64    `db:"message_telegram_id"`
 	FromUserTelegramID uint64    `db:"from_user_telegram_id"`
 	InTelegramChatID   int64     `db:"in_telegram_chat_id"`
 	MessageText        string    `db:"message_text"`

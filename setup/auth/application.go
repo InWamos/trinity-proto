c7ea3c60e27b76598a7fda11ac095b28c470731b
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-20 13:47:40 +0000

    add: fx dependencies for new session management endpoint

--- setup/auth/application.go
+++ setup/auth/application.go
@@ -14,6 +14,7 @@ func NewAuthApplicationContainer() fx.Option {
 			// Provides VerifySession interactor
 			application.NewVerifySession,
 			application.NewRemoveSession,
+			application.NewGetAllSessionsByUserID,
 		),
 	)
 }
--- setup/auth/presentation.go
+++ setup/auth/presentation.go
@@ -20,8 +20,12 @@ func NewAuthPresentationContainer() fx.Option {
 			handlers.NewLoginHandler,
 			// Provides logout handler
 			handlers.NewLogoutHandler,
+			// Provides session getter handler
+			handlers.NewGetAllSessionsByUserIDHandler,
 			// Provides auth multiplexer with routes
 			authv1mux.NewAuthMuxV1,
+			// Provides session management endpoints
+			authv1mux.NewSessionManagementMuxV1,
 		),
 	)
 }

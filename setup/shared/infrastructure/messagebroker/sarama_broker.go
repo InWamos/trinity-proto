64c026ace00dccdb966cd7a84e558d78db9bd2d1
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-22 18:50:16 +0000

    add: sarama bus

---
+++ setup/shared/infrastructure/messagebroker/sarama_broker.go
@@ -0,0 +1,17 @@
+package messagebroker
+
+import (
+	"github.com/InWamos/trinity-proto/internal/shared/infrastructure/broker/kafka/saramabroker"
+	"go.uber.org/fx"
+)
+
+func NewSqlxDatabaseContainer() fx.Option {
+	return fx.Module(
+		"sarama_broker",
+		fx.Provide(
+			saramabroker.NewSaramaBroker,
+			func(broker *saramabroker.SaramaBroker) saramabroker.UserSyncProducer {
+				return broker.GetSyncUserProducer()
+			},
+		))
+}

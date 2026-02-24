8a2d121250a970d9e7a974589f76a1ad973488c3
InWamos <74014248+InWamos@users.noreply.github.com>
2026-02-24 12:12:06 +0000

    add: lifecycle hooks for the sarama

--- setup/shared/infrastructure/messagebroker/sarama_broker.go
+++ setup/shared/infrastructure/messagebroker/sarama_broker.go
@@ -1,6 +1,9 @@
 package messagebroker
 
 import (
+	"context"
+
+	"github.com/InWamos/trinity-proto/internal/auth/infrastructure/messaging/saramahandlers"
 	"github.com/InWamos/trinity-proto/internal/shared/infrastructure/broker/kafka/saramabroker"
 	"go.uber.org/fx"
 )
@@ -10,11 +13,32 @@ func NewSaramaBrokerContainer() fx.Option {
 		"sarama_broker",
 		fx.Provide(
 			saramabroker.NewSaramaBroker,
+		),
+		fx.Invoke(
+			func(lc fx.Lifecycle, broker *saramabroker.SaramaBroker, authConsumerGroup saramabroker.AuthConsumer, authConsumerHandler *saramahandlers.AuthConsumerGroupHandler) {
+				lc.Append(fx.Hook{
+					OnStart: func(ctx context.Context) error {
+						authHandlerTopics := []string{"user-events"}
+						go authConsumerGroup.Consume( //nolint: errcheck // check for err value is not needed
+							context.Background(),
+							authHandlerTopics,
+							authConsumerHandler,
+						)
+						return nil
+					},
+					OnStop: func(ctx context.Context) error {
+						return broker.Close()
+					},
+				})
+			},
+		),
+		fx.Provide(
 			func(broker *saramabroker.SaramaBroker) saramabroker.UserSyncProducer {
 				return broker.GetSyncUserProducer()
 			},
 			func(broker *saramabroker.SaramaBroker) saramabroker.AuthConsumer {
 				return broker.GetAuthConsumerGroup()
 			},
-		))
+		),
+	)
 }
